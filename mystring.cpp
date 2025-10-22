e7874775579faf0d2c6d62c485b96283a94e50da
Martin Jedla <jedlam02@gmail.com>
2025-10-22 19:14:55 +0200

    Commit

--- mystring.cpp
+++ mystring.cpp
@@ -1,8 +1,11 @@
 #include "mystring.h"
 
 MyString::StringData::StringData() : data_ {nullptr}, count_ {0} {}
+
 MyString::StringData::StringData(const char* array) {
-    data_ = new char[strlen(array) + 1];
+    length_ = strlen(array);
+    data_ = new char[length_ + 1];
+    strcpy(data_, array);
     count_ = 1;
 }
 
@@ -15,36 +18,81 @@ MyString::StringData::~StringData() {
 }
 
 MyString::MyString() : string {nullptr} {}
+
 MyString::MyString(const char* array) {
     string = new MyString::StringData(array);
+}
+
+MyString::MyString(const MyString& myString) {
+    string = myString.string;
+    ++string->count_;
 }
 
 MyString& MyString::operator=(MyString& other) {
     string = other.string;
     ++string->count_;
+    return *this;
 }
 
 const char& MyString::operator[](const uint32_t i) const {
     return string->data_[i];
 }
 
-char& MyString::operator[](const uint32_t i) {}
+char& MyString::operator[](const uint32_t i) {
+    release();
+    string = new MyString::StringData(string->data_);
+    return string->data_[i];
+}
 
 const char* MyString::c_str() const { return string->data_; }
 
 MyString& MyString::operator=(const char* other) {
     if(string) {
-        if(string->count_ == 1) {
-            delete string;
-        } else {
-            --string->count_;
-        }
+        release();
     }
     string = new MyString::StringData(other);
     return *this;
 }
 
-std::ostream& operator<<(std::ostream& os, MyString& myString) {
+std::ostream& operator<<(std::ostream& os, const MyString& myString) {
     os << myString.c_str();
     return os;
 }
+
+void MyString::release() {
+    if(string->count_ == 1) {
+        delete string;
+        return;
+    }
+    --string->count_;
+}
+
+MyString operator+(const MyString& lhs, const MyString& rhs) {
+    uint32_t newLength = string->length_ + other.string->length_;
+    char* newArray = new char[newLength + 1];
+    strcpy(newArray, string->data_);
+    strcat(newArray, other.string->data_);
+    StringData* newStringData = new StringData(newArray);
+    delete[] newArray;
+
+    release();
+    string = newStringData;
+    return *this;
+}
+MyString operator+(const MyString& lhs, char rhs) {
+    uint32_t newLength = string->length_ + 1;
+    char* newArray = new char[newLength + 1];
+    strcpy(newArray, string->data_);
+    newArray[newLength - 1] = rhs;
+    newArray[newLength] = '\0';
+    StringData* newStringData = new StringData(newArray);
+    delete[] newArray;
+
+    release();
+    string = newStringData;
+    return *this;
+}
+MyString& MyString::operator+=(const MyString& other);
+MyString& MyString::operator+=(const char other);
+
+uint32_t length() const;
--- mystring.h
+++ mystring.h
@@ -7,15 +7,21 @@ class MyString {
 public:
     MyString();
     MyString(const char* array);
+    MyString(const MyString& array);
     MyString& operator=(MyString& other);
     MyString& operator=(const char* other);
     const char& operator[](uint32_t i) const;
     char& operator[](uint32_t i);
     const char* c_str() const;
+    MyString& operator+=(const MyString& other);
+    MyString& operator+=(const char other);
+
+    uint32_t length() const;
 
 private:
     struct StringData {
         char* data_;
+        uint32_t length_;
         uint32_t count_;
         StringData();
         StringData(const char* array);
@@ -24,6 +30,11 @@ private:
     };
 
     StringData* string;
+    void release();
 };
 
-std::ostream& operator<<(std::ostream& os, MyString& myString);
+MyString operator+(const MyString& lhs, const MyString& rhs);
+MyString operator+(const MyString& lhs, char rhs);
+MyString operator+(char lhs, const MyString& rhs);
+std::ostream& operator<<(std::ostream& os, const MyString& myString);
+std::istream& operator>>(std::istream& is, MyString& myString);
